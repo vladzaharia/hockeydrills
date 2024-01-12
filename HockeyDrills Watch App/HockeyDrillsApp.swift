21c119081b903c8b49bdaca21210e2fcf7fcd51e
Vlad Zaharia <hey@vlad.gg>
2024-01-11 23:23:12 -0800

    Initial Commit

---
+++ HockeyDrills Watch App/HockeyDrillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyDrillsApp.swift
+//  HockeyDrills Watch App
+//
+//  Created by Vlad Zaharia on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HockeyDrills_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
