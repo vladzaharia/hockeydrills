ed741980309b9af51db0b816417422362877a380
Vlad Zaharia <hey@vlad.gg>
2024-01-14 03:08:22 -0800

    Settings UI for managing which drill config to use

---
+++ HockeyDrills Watch App/Models/SettingsManager.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsManager.swift
+//  HockeyDrills Watch App
+//
+//  Created by Vlad Zaharia on 1/14/24.
+//
+
+import Foundation
---
+++ HockeyDrills Watch App/Views/Start/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  HockeyDrills Watch App
+//
+//  Created by Vlad Zaharia on 1/14/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
